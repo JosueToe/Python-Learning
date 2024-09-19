ac8d60443b116957b50bd6f94db63b2b1cca011b
JosueToe <jtole043@fiu.edu>
2024-09-18 21:33:33 -0400

    First commit Lesson 1 of python
    This is the start of Python learning

---
+++ Lesson1.py
@@ -0,0 +1,26 @@
+
+
+#print("Hello World!!!!!!!!!!")
+print("I am Iron Man1")
+print("No, I am Tony Stark1")
+print("No, I am Poppy1")
+# We are running python Strings/Print Function
+
+#To have a multi line string use three """
+print("""
+I am Iron Man2.
+No, I am Tony Stark2.
+No, I am Poppy2.""")
+
+#Another way to put strings together in one line (Concatanating Strings)
+print("")
+print("I am Iron Man3." + " No, I am Tony Stark3." + " No, I am Poppy3.")
+
+#Another way to put strings together but on seperate lines between sentences \n
+print("")
+print("I am Iron Man4\n" + "No, I am Tony Stark4.\n" + "No, I am Poppy4.\n")
+
+#Another function with strings but multiplying its print
+print("")
+print("No, I am Poppy5.\n" * 4)
+
